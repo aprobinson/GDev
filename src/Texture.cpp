f4c26f485772cda16af9bdbd2bc4a6247de5ac22
Alex Robinson <aprobinson@wisc.edu>
2015-10-10 23:16:32 -0500

    Finished testing the static texture class.

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -144,13 +144,13 @@ Texture::~Texture()
 }
 
 // Get the width of the texture
-unsigned Texture::getWidth() const
+int Texture::getWidth() const
 {
   return d_width;
 }
 
 // Get the height of the texture
-unsigned Texture::getHeight() const
+int Texture::getHeight() const
 {
   return d_height;
 }
@@ -241,6 +241,8 @@ void Texture::setBlendMode( const SDL_BlendMode mode )
 }
 
 // Get the texture format
+/*! \details This is the format that should be used with pixel transfers.
+ */
 Uint32 Texture::getFormat() const
 {
   return d_format;
@@ -259,12 +261,13 @@ SDL_Texture* Texture::getRawTexturePtr()
 }
 
 // Render the texture with default parameters
-void Texture::render()
+void Texture::render() const
 {
-  int return_value = SDL_RenderCopy( d_renderer->getRawRendererPtr(),
-				     d_texture,
-				     NULL,
-				     NULL );
+  int return_value = 
+    SDL_RenderCopy( const_cast<SDL_Renderer*>(d_renderer->getRawRendererPtr()),
+		    const_cast<SDL_Texture*>(d_texture),
+		    NULL,
+		    NULL );
 
   TEST_FOR_EXCEPTION( return_value != 0,
 		      ExceptionType,
@@ -272,46 +275,56 @@ void Texture::render()
 		      "SDL_Error: " << SDL_GetError() );
 }
 
-// Render the texture will user parameters
-void Texture::render( const int x_position,
-		      const int y_position,
+// Render the whole texture clip at the desired point
+/*! \details The rotation center is the point on the target around which
+ * the target clip will be rotated. The default is the center of the target.
+ */
+void Texture::render( const int target_x_position,
+		      const int target_y_position,
 		      const SDL_Rect* texture_clip,
 		      const double rotation_angle,
 		      const SDL_Point* rotation_center,
-		      const SDL_RendererFlip flip )
+		      const SDL_RendererFlip flip ) const
 {
   // Set the target rectangle where the texture clip will be rendered
-  SDL_Rect target_rect = { x_position, y_position, d_width, d_height };
+  SDL_Rect target_clip = {target_x_position, 
+			  target_y_position, 
+			  d_width, 
+			  d_height};
 			   
 
   // Set the clip rendering dimensions
   if( texture_clip != NULL )
   {
-    target_rect.w = texture_clip->w;
-    target_rect.h = texture_clip->h;
+    target_clip.w = texture_clip->w;
+    target_clip.h = texture_clip->h;
   }
 
-  this->render( texture_clip, 
-		&target_rect, 
+  this->render( &target_clip, 
+		texture_clip,
 		rotation_angle, 
 		rotation_center, 
 		flip );
 }
 
 // Render the texture
-void Texture::render( const SDL_Rect* texture_rect,
-		      const SDL_Rect* target_rect,
+/*! \details The rotation center is the point on the target around which
+ * the target clip will be rotated. The default is the center of the target.
+ */
+void Texture::render( const SDL_Rect* target_clip,
+		      const SDL_Rect* texture_clip,
 		      const double rotation_angle,
 		      const SDL_Point* rotation_center,
-		      const SDL_RendererFlip flip )
+		      const SDL_RendererFlip flip ) const
 {
-  int return_value = SDL_RenderCopyEx( d_renderer->getRawRendererPtr(),
-				       d_texture,
-				       texture_rect,
-				       target_rect,
-				       rotation_angle,
-				       rotation_center,
-				       flip );
+  int return_value = SDL_RenderCopyEx(
+		    const_cast<SDL_Renderer*>(d_renderer->getRawRendererPtr()),
+		    const_cast<SDL_Texture*>(d_texture),
+		    texture_clip,
+		    target_clip,
+		    rotation_angle,
+		    rotation_center,
+		    flip );
 
   TEST_FOR_EXCEPTION( return_value != 0,
 		      ExceptionType,
